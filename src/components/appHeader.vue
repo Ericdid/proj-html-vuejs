f96d8dcf02f6b7f57834278944f36a2d0cae6cbb
Ericdid <ericdidom@gmail.com>
2023-09-19 19:55:22 +0200

    add.startHeaderBottom

--- src/components/appHeader.vue
+++ src/components/appHeader.vue
@@ -3,35 +3,50 @@ export default {};
 </script>
 
 <template>
-  <div class="d-flex header">
-    <!-- this is the green header with company Info -->
-    <div class="header-top d-flex justify-content-around align-items-center">
-      <!-- left menu with location and Email Adress -->
-      <div class="left-menu d-flex">
-        <div class="me-3">
-          <font-awesome-icon icon="fa-solid fa-location-dot" />
-          Location
-        </div>
-        <div>
-          <font-awesome-icon icon="fa-solid fa-envelope" />
-          Email Adress@demo.com
-        </div>
+  <!-- this is the green header with company Info -->
+  <div class="header-top d-flex justify-content-around align-items-center">
+    <!-- left menu with location and Email Adress -->
+    <div class="left-tp-menu d-flex">
+      <div class="me-3">
+        <font-awesome-icon icon="fa-solid fa-location-dot" />
+        Location
       </div>
-      <!-- this is the right menu with working hours and social pages -->
-      <div class="right-menu d-flex">
-        <div class="me-3">
-          <font-awesome-icon icon="fa-regular fa-clock" />
-          8.30AM-8.30PM
-        </div>
-        <div>
-          <font-awesome-icon class="me-2" icon="fa-brands fa-facebook-f" />
-          <font-awesome-icon class="me-2" icon="fa-brands fa-twitter" />
-          <font-awesome-icon class="me-2" icon="fa-brands fa-instagram" />
-          <font-awesome-icon class="me-2" icon="fa-brands fa-pinterest-p" />
-        </div>
+      <div>
+        <font-awesome-icon icon="fa-solid fa-envelope" />
+        Email Adress@demo.com
       </div>
     </div>
-    <div class="header-bottom"></div>
+    <!-- this is the right menu with working hours and social pages -->
+    <div class="right-tp-menu d-flex">
+      <div class="me-3">
+        <font-awesome-icon icon="fa-regular fa-clock" />
+        8.30AM-8.30PM
+      </div>
+      <div>
+        <font-awesome-icon class="me-2" icon="fa-brands fa-facebook-f" />
+        <font-awesome-icon class="me-2" icon="fa-brands fa-twitter" />
+        <font-awesome-icon class="me-2" icon="fa-brands fa-instagram" />
+        <font-awesome-icon class="me-2" icon="fa-brands fa-pinterest-p" />
+      </div>
+    </div>
+  </div>
+  <!-- this is the header with navigation bar and shopping data -->
+  <div class="header-bottom d-flex justify-content-around align-items-center">
+    <div class="left-bt-menu d-flex">
+      <img src="../assets/img/logo.png" alt="" />
+      <div class="d-flex">
+        <ul class="d-flex align-items-center h-100">
+          <li>prova</li>
+          <li>prova</li>
+          <li>prova</li>
+          <li>prova</li>
+          <li>prova</li>
+          <li>prova</li>
+          <li>prova</li>
+        </ul>
+      </div>
+    </div>
+    <div class="right-bt-menu">placeholder</div>
   </div>
 </template>
 
@@ -41,5 +56,16 @@ export default {};
   color: white;
   width: 100%;
   height: 40px;
+}
+.header-bottom {
+  height: 100px;
+  background-color: aquamarine;
+  ul {
+    list-style-type: none;
+    li {
+      padding: 0 5px;
+      cursor: pointer;
+    }
+  }
 }
 </style>
