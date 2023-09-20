383ce951002d2ba9600ce163b6d4d705a88ef8d3
Ericdid <ericdidom@gmail.com>
2023-09-20 22:56:35 +0200

    add.txt-lines

--- src/components/appMain.vue
+++ src/components/appMain.vue
@@ -7,6 +7,7 @@ export default {
 </script>
 
 <template>
+  <!-- this is the jumbotron -->
   <div
     class="jumbotron d-flex justify-content-center align-items-center text-center"
   >
@@ -24,10 +25,32 @@ export default {
     <span class="arw-left">&#10094;</span>
     <span class="arw-right">&#10095;</span>
   </div>
+  <!-- this is the section with product info -->
+  <div
+    class="d-flex justify-content-center align-items-center text-center product-info"
+  >
+    <div class="container d-flex product-text">
+      <div class="large-txt">
+        <span class="gold-txt">Lorem ipsum dolor</span> sit amet consectetur
+        adipisicing elit. Eaque porro id ipsa
+        <span class="gold-txt">labore expedita iusto</span> blanditiis dicta
+        error distinctio quis!
+      </div>
+      <div class="small-txt">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eum
+        praesentium suscipit in blanditiis esse neque quo! Praesentium dolor
+        iusto Lorem ipsum dolor sit amet,
+        <span class="gold-txt">consectetur adipisicing elit.</span>
+        <br />
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam
+        exercitationem asperiores ea animi perspiciatis
+      </div>
+    </div>
+  </div>
 </template>
 
 <style lang="scss" scoped>
-// this is the jumbotron and elements inside of it
+// this is the style of the jumbotron and elements inside of it
 .jumbotron {
   background-image: url("../assets/img/H3-slider3.jpg");
   height: 800px;
@@ -57,7 +80,7 @@ export default {
     background-color: goldenrod;
   }
 }
-// those are the arrows for the carousel
+// those are the style of the arrows for the carousel
 .arw-left {
   position: absolute;
   cursor: pointer;
@@ -71,5 +94,29 @@ export default {
   padding: 30px 20px;
   right: 15px;
   top: 50%;
+}
+// this is the style of the product info
+.product-info {
+  height: 500px;
+  .product-text {
+    height: 50%;
+    color: rgb(37, 116, 102);
+    font-weight: 900;
+    font-size: x-large;
+    .large-txt {
+      width: 50%;
+      text-align: left;
+    }
+    .gold-txt {
+      color: goldenrod;
+    }
+    .small-txt {
+      width: 50%;
+      text-align: left;
+      color: black;
+      font-size: medium;
+      font-weight: 100;
+    }
+  }
 }
 </style>
